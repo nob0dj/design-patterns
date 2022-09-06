e990116c0de011504d25222e19a049f047d4d420
nob0dj <dongxuan1863@gmail.com>
2022-09-06 21:31:38 +0900

    factory-method init!

---
+++ factory-method/src/main/java/com/sh/factory/method/level1/Duck.java
@@ -0,0 +1,17 @@
+package com.sh.factory.method.level1;
+
+public class Duck {
+
+}
+
+class RubberDuck extends Duck {
+	
+}
+
+class QuackDuck extends Duck {
+	
+}
+
+class MallardDuck extends Duck {
+	
+}
---
+++ factory-method/src/main/java/com/sh/factory/method/level2/SuperDuckFactory.java
@@ -0,0 +1,9 @@
+package com.sh.factory.method.level2;
+
+import com.sh.factory.method.level1.Duck;
+
+public abstract class SuperDuckFactory {
+
+	public abstract Duck createDuck(int type);
+	
+}
