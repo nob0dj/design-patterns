4a20dad8bccd22ddf67f5b94ce1f7562f0ac6670
nob0dj <dongxuan1863@gmail.com>
2022-09-06 21:35:14 +0900

    singleton init!

---
+++ singleton/src/main/java/com/sh/singleton/eager/StaticBlockSingleton.java
@@ -0,0 +1,20 @@
+package com.sh.singleton.eager;
+
+/**
+ * field로 선언시 객체를 할당하는 스타일
+ */
+public class StaticBlockSingleton {
+
+	private static final StaticBlockSingleton instance = new StaticBlockSingleton();
+
+	/**
+	 *  private constructor to avoid client applications to use constructor
+	 */
+	private StaticBlockSingleton() {
+		
+	}
+
+	public static StaticBlockSingleton getInstance() {
+		return instance;
+	}
+}
