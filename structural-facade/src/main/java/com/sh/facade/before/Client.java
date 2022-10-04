bec9cf29830c60897cc8bddf2f339f71f015b0ff
nob0dj <dongxuan1863@gmail.com>
2022-10-04 22:55:51 +0900

    structural-facade Email 추상화한 facade패턴 적용

---
+++ structural-facade/src/main/java/com/sh/facade/before/Client.java
@@ -0,0 +1,40 @@
+package com.sh.facade.before;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+/**
+ * javax.mail 라이브러리에 지나치게 의존함.
+ *
+ * Properties -> Session -> MimeMessage -> Transport.send
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        String to = "keesun@whiteship.me";
+        String from = "whiteship@whiteship.me";
+        String host = "127.0.0.1";
+
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", host);
+
+        Session session = Session.getDefaultInstance(properties);
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("Test Mail from Java Program");
+            message.setText("message");
+
+            Transport.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
