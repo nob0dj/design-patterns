16cd6c338d1090977a3df959a6b00c8de6139eeb
nob0dj <dongxuan1863@gmail.com>
2022-10-27 19:25:11 +0900

    책임연쇄패턴 추가
    - 책임연쇄패턴 적용전 상속을 이용한 버젼

--- behavioral-chain-of-responsibility/src/main/java/com/sh/chain/of/responsibility/before/Client.java
+++ behavioral-chain-of-responsibility/src/main/java/com/sh/chain/of/responsibility/before/Client.java
@@ -4,7 +4,9 @@ public class Client {
 
     public static void main(String[] args) {
         Request request = new Request("무궁화 꽃이 피었습니다.");
-        RequestHandler requestHandler = new RequestHandler();
+//        RequestHandler requestHandler = new RequestHandler();
+//        RequestHandler requestHandler = new AuthRequestHandler();
+        RequestHandler requestHandler = new LoggingRequestHandler();
         requestHandler.handle(request);
     }
 }
