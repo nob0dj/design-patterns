13ab918f4abdc1b95f909249f98b7347ffb20370
nob0dj <dongxuan1863@gmail.com>
2022-10-28 20:19:19 +0900

    자바 책임연쇄패턴 - Filter Chain

---
+++ behavioral-chain-of-responsibility/src/main/java/com/sh/chain/of/responsibility/usage/java/ChainOfResponsibilityInJava.java
@@ -0,0 +1,18 @@
+package com.sh.chain.of.responsibility.usage.java;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class ChainOfResponsibilityInJava {
+
+    public static void main(String[] args) {
+        Filter filter = new Filter() {
+            @Override
+            public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+                // TODO 전처리
+                chain.doFilter(request, response);
+                // TODO 후처리
+            }
+        };
+    }
+}
