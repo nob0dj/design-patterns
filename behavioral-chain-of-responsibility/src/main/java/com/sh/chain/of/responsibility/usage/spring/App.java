2a3ec2447d7ada2071a1aab21430daf36c58ace5
nob0dj <dongxuan1863@gmail.com>
2022-10-29 15:07:27 +0900

    책임연쇄패턴 in spring
    - ServletFilter
    - SecurityConfig

---
+++ behavioral-chain-of-responsibility/src/main/java/com/sh/chain/of/responsibility/usage/spring/App.java
@@ -0,0 +1,14 @@
+package com.sh.chain.of.responsibility.usage.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
