69e7aa3c01a403ddf5324c7e9cc3aa0f3792ebb1
nob0dj <dongxuan1863@gmail.com>
2022-10-31 19:48:43 +0900

    just refactoring

--- behavioral-command/src/main/java/com/sh/command/after/LightOffCommand.java
+++ behavioral-command/src/main/java/com/sh/command/after/LightOffCommand.java
@@ -2,6 +2,7 @@ package com.sh.command.after;
 
 public class LightOffCommand implements Command {
     private Light light;
+
     public LightOffCommand(Light light) {
         this.light = light;
     }
