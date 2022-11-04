8a469f30fde85f9dec0fbf1619bbeaa070e69559
nob0dj <dongxuan1863@gmail.com>
2022-11-04 20:19:19 +0900

    iterator pattern 시작

---
+++ behavioral-iterator/src/main/java/com/sh/iterator/before/Board.java
@@ -0,0 +1,27 @@
+package com.sh.iterator.before;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class Board {
+
+    /**
+     * List<Post> : 이때 List 인터페이스가 Aggregate 인터페이스 역할
+     *
+     * ArrayList : ConcreteAggregate
+     */
+    List<Post> posts = new ArrayList<>();
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    public void addPost(String content) {
+        this.posts.add(new Post(content));
+    }
+}
