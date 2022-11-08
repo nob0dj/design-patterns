8897104228a11185db601a8f64354dd81b22abb7
nob0dj <dongxuan1863@gmail.com>
2022-11-08 18:54:44 +0900

    iterator pattern 사용법
    - Enumeration -> Iterator

---
+++ behavioral-iterator/src/main/java/com/sh/iterator/usage/spring/IteratorInSpring.java
@@ -0,0 +1,10 @@
+package com.sh.iterator.usage.spring;
+
+import org.springframework.util.CompositeIterator;
+
+public class IteratorInSpring {
+
+    public static void main(String[] args) {
+        CompositeIterator iterator;
+    }
+}
