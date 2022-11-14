347ca851d7936d06c4fd2032f301ae56103e44c5
nob0dj <dongxuan1863@gmail.com>
2022-11-14 09:52:26 +0900

    memento 패턴 추가

---
+++ behavioral-memento/src/main/java/com/sh/memento/before/Client.java
@@ -0,0 +1,18 @@
+package com.sh.memento.before;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.setRedTeamScore(10);
+        game.setBlueTeamScore(20);
+
+        // 클라이언트 쪽에 Game객체 내부를 모두 노출하게 되고, 캡슐화가 깨진다.
+        int blueTeamScore = game.getBlueTeamScore();
+        int redTeamScore = game.getRedTeamScore();
+
+        Game restoredGame = new Game();
+        restoredGame.setBlueTeamScore(blueTeamScore);
+        restoredGame.setRedTeamScore(redTeamScore);
+    }
+}
