92f09e48bf124fb3be9630b9b325127c87661201
nob0dj <dongxuan1863@gmail.com>
2022-11-14 09:39:58 +0900

    iterator pattern after패키지 - refactoring

---
+++ behavioral-mediator/src/main/java/com/sh/mediator/after/FrontDesk.java
@@ -0,0 +1,37 @@
+package com.sh.mediator.after;
+
+
+import java.time.LocalDateTime;
+
+/**
+ * concreate mediator로써 mediator 인터페이스는 사용하지 않았다.
+ */
+public class FrontDesk {
+    private CleaningService cleaningService;
+    private Restaurant restaurant;
+
+    /**
+     * cleaningService.getTowels 호출시 의존성이 없도록 Room객체가 아닌 id값을 넘긴다.
+     * @param guest
+     * @param numOfTowels
+     */
+    public void getTowels(Guest guest, int numOfTowels) {
+        this.cleaningService.getTowels(guest.getRoomId(), numOfTowels);
+    }
+
+    public void bookDinner(Guest guest, LocalDateTime when) {
+        this.restaurant.bookDinner(guest.getRoomId(), when);
+    }
+
+    public void setCleaningService(CleaningService cleaningService) {
+        this.cleaningService = cleaningService;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public void clean(Restaurant restaurant) {
+        this.cleaningService.clean(restaurant.getRestaurantId());
+    }
+}
