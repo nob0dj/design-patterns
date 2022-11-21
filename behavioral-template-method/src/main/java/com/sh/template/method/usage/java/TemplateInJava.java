c85a0d62b7aa56dfc9eba0c8d51e9fa7578e3801
nob0dj <dongxuan1863@gmail.com>
2022-11-21 18:11:42 +0900

    behavioral-template-method usage
    - java : HttpServlet
    - spring : SecurityConfiguration
    - spring : JdbcTemplate
    - spring : RestTemplate

---
+++ behavioral-template-method/src/main/java/com/sh/template/method/usage/java/TemplateInJava.java
@@ -0,0 +1,10 @@
+package com.sh.template.method.usage.java;
+
+import javax.servlet.http.HttpServlet;
+
+public class TemplateInJava {
+
+    public static void main(String[] args) {
+        HttpServlet httpServlet;
+    }
+}
